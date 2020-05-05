7cf41597151891e2b1da6800223591e8de230242
Param Harrison <paramanantham@live.com>
2020-05-05 23:23:11 +0300

    feat: styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,12 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
 
-      <Flex flexDirection="column" minH="400px" justifyContent="center">
+      <Flex
+        bg="green.50"
+        flexDirection="column"
+        minH="450px"
+        justifyContent="center"
+      >
         <Box p={4} maxW="600px" mx="auto" textAlign="center">
           <Heading as="h1" fontSize="36px" textTransform="uppercase">
             Building learning habits for kids
@@ -33,13 +38,17 @@ const IndexPage = () => {
       <Flex
         maxW="720px"
         mx="auto"
+        my={6}
         px={6}
         flexDirection="column"
         justifyContent="center"
       >
-        <Box my={3} p={6} bg="red.100" borderRadius="4px">
+        <Heading as="h2" my={6} textAlign="center">
+          Questions
+        </Heading>
+        <Box my={3} p={6} bg="purple.100" borderRadius="4px">
           <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            Question 1: Simplify and find the answer for this math
+            1: Simplify and find the answer for this math
           </Heading>
           <Text fontSize="20px" mt={3}>
             {`36 − [18 − (15 − 4 ÷ 2 × 2)]`} =
@@ -47,7 +56,7 @@ const IndexPage = () => {
         </Box>
         <Box my={3} p={6} bg="green.100" borderRadius="4px">
           <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            Question 2: Open science question
+            2: Open science question
           </Heading>
           <Text fontSize="20px" mt={3}>
             About three-fourths of the earth's surface is covered with water.
@@ -56,21 +65,30 @@ const IndexPage = () => {
         </Box>
         <Box my={3} p={6} bg="blue.100" borderRadius="4px">
           <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            Question 3: Challenge
+            3: Maths Challenge
           </Heading>
           <Text fontSize="20px" mt={3}>
-            The length of a cloth is 20 CM. You have 10 M wire for hanging those
-            clothes.
             <Text>
-              Q: What is the maximum number of clothes you can hang in the wire
-              anytime?
+              The length of a cloth is 7 CM. You have 10 M wire for hanging
+              those clothes.
+            </Text>
+            <br />
+            <Text>
+              Q 1: What is the maximum number of clothes you can hang in the
+              wire anytime?
+            </Text>
+            <br />
+            <Text>
+              Q 2: How many CM remaining after maximum number of clothes are
+              hanged?
             </Text>
           </Text>
         </Box>
       </Flex>
       <Flex
+        bg="pink.50"
         flexDirection="column"
-        minH="400px"
+        minH="450px"
         justifyContent="center"
         alignItems="center"
       >
@@ -78,22 +96,27 @@ const IndexPage = () => {
           <List m={6}>
             <ListItem fontSize="24px" m={3}>
               <ListIcon icon="check-circle" color="green.300" />
+              {` `}
               Learning everyday is important.
             </ListItem>
             <ListItem fontSize="24px" m={3}>
               <ListIcon icon="check-circle" color="green.300" />
+              {` `}
               The process to find the answer is more important.
             </ListItem>
             <ListItem fontSize="24px" m={3}>
               <ListIcon icon="check-circle" color="green.300" />
+              {` `}
               No answer is perfect.
             </ListItem>
             <ListItem fontSize="24px" m={3}>
               <ListIcon icon="check-circle" color="green.300" />
+              {` `}
               If possible, record and share your answers everyday to youtube.
             </ListItem>
             <ListItem fontSize="24px" m={3}>
               <ListIcon icon="check-circle" color="green.300" />
+              {` `}
               These questions helps to build a learning habit.
             </ListItem>
           </List>
