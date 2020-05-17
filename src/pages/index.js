2d17360feca68ff34bb305ff836fa3a4f137a5f5
Param Harrison <paramanantham@live.com>
2020-05-17 22:35:43 +0300

    feat: 18 may 2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,25 +52,28 @@ const IndexPage = () => {
           </Heading>
           <Text fontSize="20px" mt={3}>
             <Text>
-              Train running at a speed of 60 KM per hour. There are three stops
-              between starting point and destination.
-              <br />
-              First stop is at 30 KM
-              <br />
-              Second stop is at 45 KM
-              <br />
-              Third stop is at 72 KM
-              <br />
+              First ship travels at a speed of 100 KM per hour. Second ship from
+              opposite side travel at a speed of 120 KM per hour. First ship
+              travels from Chennai to Andaman. Second ship travels from Andaman
+              to Chennai. They both start their journey at the same time at 5 PM
+              in the evening. The total distance between Andaman and Chennai is
+              560 KM.
             </Text>
             <Text>
-              Q 1: How long does it takes for the train to reach 1st, 2nd and
-              3rd stop?
+              Q 1: How long does it takes for the first ship to reach Andaman?
+            </Text>
+            <Text>
+              Q 2: How long does it takes for the second ship to reach Chennai?
+            </Text>
+            <Text>
+              Q 3: When does both the ship crosses each other? (in time)
             </Text>
           </Text>
         </Box>
         <Box my={3} p={6} bg="green.100" borderRadius="4px">
-          <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            2: Speak about today's news for 10 mins
+          <Heading as="h2" fontSize="24px">
+            2: Create a dialogue between you and a restaurant waiter (atleast 10
+            conversation)
           </Heading>
         </Box>
         <Box my={3} p={6} bg="blue.100" borderRadius="4px">
@@ -79,7 +82,7 @@ const IndexPage = () => {
           </Heading>
           <Box mt={3}>
             <Image
-              src="https://1.bp.blogspot.com/-SHl5U4FPMAU/Xq15aQlhnvI/AAAAAAAAgPg/DLUDXJoB19w8ZrXZSvxHzdRCeLEU3SDRACLcBGAsYHQ/s1600/Juice%2BSandwich%2BMango%2BEquation%2BPuzzle.png"
+              src="https://images.squarespace-cdn.com/content/v1/54905286e4b050812345644c/1586987812039-9B8C3ZSUN13L5DZ1HVPN/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/one.jpg?format=2500w"
               alt="number-puzzle"
             />
           </Box>
