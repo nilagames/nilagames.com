9171245ab5933d38533b10ea6bb27b52ac410711
Param Harrison <paramanantham@live.com>
2020-05-27 09:31:09 +0300

    feat: 27-05-2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,15 +48,31 @@ const IndexPage = () => {
         </Heading>
         <Box my={3} p={6} bg="purple.100" borderRadius="4px">
           <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            1: Reading the story from URL
+            1: Reading the story
           </Heading>
           <Text fontSize="20px" mt={3}>
-            <a
-              target="_blank"
-              href="https://www.bbc.co.uk/learningenglish/english/features/childrens-stories/therobot"
-            >
-              Read the story here
-            </a>
+            The lemon is a small tree that is green even in the winter. It came
+            from Asia, and is also the name of the tree's oval-shaped yellow
+            fruit. The fruit is used for cooking and other things in the world –
+            usually for its juice. People do not know for sure where lemons have
+            come from. However, most people think that lemons first grew in
+            India, northern Burma, and China. The lemon is the common name for
+            Citrus limon. A lemon is a yellow citrus fruit. It is related to the
+            orange. Lemon juice is about 5% citric acid, and has a pH of 2 to 3.
+            Lemon plants vary in size yet stay generally small. The tallest
+            height they can get is about 6 meters tall. Lemons taste sour. The
+            juice, zest, and pulp are often used in cooking, often on fish and
+            other meat for better taste. Lemon is also used to flavour drinks,
+            such as lemonade or soft drinks.
+          </Text>
+          <Text fontSize="20px" mt={2}>
+            Q 1: How tall lemon can grow?
+          </Text>
+          <Text fontSize="20px" mt={2}>
+            Q 2: What is the taste of lemon?
+          </Text>
+          <Text fontSize="20px" mt={2}>
+            Q 2: What is the color of lemon tree during winter?
           </Text>
         </Box>
         <Box my={3} p={6} bg="green.100" borderRadius="4px">
@@ -64,43 +80,25 @@ const IndexPage = () => {
             2: Math puzzle
           </Heading>
           <Text fontSize="20px" mt={3}>
-            In a survey of university students, 64 had taken mathematics course,
-            94 had taken chemistry course, 58 had taken physics course, 28 had
-            taken mathematics and physics, 26 had taken mathematics and
-            chemistry, 22 had taken chemistry and physics course, and 14 had
-            taken all the three courses.
+            In a class of 60 students, 40 students like math, 36 like science,
+            24 like both the subjects. Find the number of students who like
+            <br />
+            (i) Math only, (ii) Science only (iii) Either Math or Science (iv)
+            Neither Math nor science.
           </Text>
           <Text mb={2}>Q 1: Find how many had taken one course only</Text>
         </Box>
-        <Box my={3} p={6} bg="blue.100" borderRadius="4px">
-          <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            3: Math challenge
-          </Heading>
-          <Text fontSize="20px" mt={3}>
-            <Text mb={2}>
-              Amul icecream company polls its customers on their favorite
-              flavor: chocolate or vanilla? 106 customers said they liked
-              chocolate, 97 customer said they like vanilla, while 31 customers
-              said they liked both chocolate and vanilla.
-            </Text>
-            <Text mb={2}>
-              Q 1: How many customers said they like only chocolate?
-            </Text>
-            <Text mb={2}>
-              Q 2: How many customers participated in the poll?
-            </Text>
-          </Text>
-        </Box>
         <Box my={3} p={6} bg="purple.100" borderRadius="4px">
           <Heading as="h2" fontSize="24px" textTransform="uppercase">
-            4: Solve it
+            3: Solve the math
           </Heading>
           <Text fontSize="20px" mt={3}>
-            Nila and Afrin invest in a business in the ratio 3 : 2. If 5% of the
-            total profit goes to charity and Nila's share is Rs. 855.
+            Nila, Afrin and Nishanth bought 3 litres of milk. The decided to
+            split the milk in ratio 4:1:3.
           </Text>
-          <Text mb={2}>Q 1: What is the total profit?</Text>
-          <Text mb={2}>Q 2: What is the Afrin's share?</Text>
+          <Text fontSize="20px" mb={2}>
+            Q 1: How much milk each one get?
+          </Text>
         </Box>
       </Flex>
 
